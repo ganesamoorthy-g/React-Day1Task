0b952d67d7c8a97a21c64e8315fe53cd61f46c7d
ganesamoorthy-g <gurunathganesh6@gmail.com>
2023-06-04 19:20:44 +0530

    Commit message

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Card from "./component/Card";
+import Header from "./component/Header";
+import NavBar from "./component/NavBar";
+
+function App() {
+  let [cartValue, setCartValue] = useState(0)
+  return <>
+    <NavBar cartValue={cartValue} />
+    <Header></Header>
+    {/* <Header /> */}
+    <section className="py-5">
+      <div className="container px-4 px-lg-5 mt-5">
+        <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+          
+          <Card setCartValue={setCartValue} />
+          <Card setCartValue={setCartValue} />
+          <Card setCartValue={setCartValue} />
+          <Card setCartValue={setCartValue} />
+        </div>
+      </div>
+    </section>
+  </>
+
+}
+
+export default App;
