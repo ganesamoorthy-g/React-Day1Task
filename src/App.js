b8d05b711d7d10a71f78367150f4f980df0f7a09
ganesamoorthy-g <gurunathganesh6@gmail.com>
2023-06-04 20:53:35 +0530

    Commit message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Sidebar from "./component/Sidebar";
+import Dashboard from "./component/Dashboard";
+function App() {
+  return <>
+    <div id="wrapper">
+        <Sidebar/>
+        <Dashboard/>
+    </div>
+  </>
+}
+
+export default App;
